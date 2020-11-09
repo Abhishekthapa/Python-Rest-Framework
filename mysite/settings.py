9cf0c1731f130b60bb0e22705c70f4d75061e537
Abhishek Thapa <acevalentyne5@gmail.com>
2020-11-09 18:22:00 +0545

    heroku working

--- mysite/settings.py
+++ mysite/settings.py
@@ -156,7 +156,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'media'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 # cdn--> content delivery network
